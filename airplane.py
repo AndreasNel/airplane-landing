ba6f51e233f628af5e6dee17e0172eb762304156
AndreasNel <nel.andreas1@gmail.com>
2018-09-10 22:58:01 +0200

    skeletong, low-level heuristics and move ops added

---
+++ airplane.py
@@ -0,0 +1,24 @@
+class Airplane:
+    def __init__(self, early, target, last, separation, early_penalty, late_penalty):
+        self.earliest_time = early
+        self.target_time = target
+        self.latest_time = last
+        self.separation_time = separation
+        self.early_penalty = early_penalty
+        self.late_penalty = late_penalty
+        self.landing_time = self.earliest_time
+
+    def valid(self):
+        """
+        Determines whether the state of the airplane is currently valid.
+        :return: Boolean, indicating whether the current state is valid.
+        """
+        return self.earliest_time <= self.landing_time <= self.latest_time
+
+    def next_available_time(self, airplane_id):
+        """
+        Gets the next available landing time for the given airplane wrt to this airplane.
+        :param airplane_id: The ID (index) of the plane to check for.
+        :return: The next available landing time for the specified airplane.
+        """
+        return self.landing_time + self.separation_time[airplane_id]
---
+++ heuristics.py
@@ -0,0 +1,53 @@
+import random
+
+
+def move_to_target(airplane):
+    """
+    Moves the landing time of the airplane closer to its target landing time.
+    :param airplane: The airplane to land.
+    """
+    airplane.landing_time = random.randint(min(airplane.landing_time, airplane.target_time), max(airplane.landing_time, airplane.target_time))
+
+
+def repel_target(airplane):
+    """
+    Moves the landing time of the airplane away from its target landing time.
+    :param airplane: The airplane to land.
+    """
+    if airplane.landing_time <= airplane.target_time:
+        land_earlier(airplane)
+    else:
+        land_later(airplane)
+
+
+def land_earlier(airplane):
+    """
+    Moves the landing time of the airplane to an earlier time.
+    :param airplane: The airplane to land.
+    """
+    airplane.landing_time = random.randint(airplane.earliest_time, airplane.landing_time)
+
+
+def land_later(airplane):
+    """
+    Moves the landing time of the airplane to a later time.
+    :param airplane: The airplane to land.
+    """
+    airplane.landing_time = random.randint(airplane.landing_time, airplane.latest_time)
+
+
+def stay_put(airplane):
+    """
+    A no-op. The airplane's landing time isn't changed.
+    :param airplane: The airplane.
+    """
+    pass
+
+
+HEURISTIC_MAP = {
+    "m": move_to_target,
+    "r": repel_target,
+    "e": land_earlier,
+    "l": land_later,
+    "s": stay_put,
+}
